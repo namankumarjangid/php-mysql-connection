09e0040cecd2a0a8d64c4c757a7db2be10de1262
Naman kumar jangid <namanjangid80162@gmail.com>
2020-09-16 10:51:49 +0530

    some php work

---
+++ basics.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<style>
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+.container{
+    max-width:80%;
+    background-color:grey;
+    margin:auto;
+}
+</style>
+<body>
+    <div class="container">
+    <h1>Lets learn about php</h1>
+    This is a container
+    <br>
+    <?php
+//     $age=7;
+//     if($age>7){
+//         echo "you are not able to come in";
+//     }
+//         else if($age==7){
+//             echo "you are able to come in";
+//         }
+//         else{
+//             echo "hello harry";
+//         }
+// $a=20;        
+// for($a=0;$a<10;$a++){
+//     echo "<br>the no is:";
+//     echo $a;
+// }
+// $str="this";
+// echo $str."<br>";
+// $lenn=strlen($str);
+// echo "the length of the striung is: ".$lenn. "<br>Thanku";
+// echo "<br>the word of this string is: ". str_word_count($str). "<br> thnkyou again";
+?>
+</div>
+</body>
+</html>
